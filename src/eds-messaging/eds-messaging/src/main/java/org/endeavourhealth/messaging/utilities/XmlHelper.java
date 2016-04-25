7375b559d3c5aa809e4602c8276c11ae8c38ccd5
jonnyry <jonny.rylands@vitalsignssoftware.com>
2016-04-25 18:51:25 +0100

    Messaging - Add Xml schema validator

--- src/eds-messaging/eds-messaging/src/main/java/org/endeavourhealth/messaging/utilities/XmlHelper.java
+++ src/eds-messaging/eds-messaging/src/main/java/org/endeavourhealth/messaging/utilities/XmlHelper.java
@@ -3,7 +3,9 @@ package org.endeavourhealth.messaging.utilities;
 import com.sun.xml.internal.ws.encoding.soap.SerializationException;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
@@ -11,7 +13,12 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.StringReader;
@@ -50,5 +57,23 @@ public class XmlHelper
     public static String getXPathString(Document document, String xpath) throws XPathExpressionException
     {
         return XPathFactory.newInstance().newXPath().evaluate(xpath, document);
+    }
+
+    public static void validate(String xml, String xsd) throws Exception
+    {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new StreamSource(new StringReader(xsd)));
+        Source xmlFile = new StreamSource(new StringReader(xml));
+
+        Validator validator = schema.newValidator();
+
+        try
+        {
+            validator.validate(xmlFile);
+        }
+        catch (SAXException e)
+        {
+            throw e;
+        }
     }
 }
