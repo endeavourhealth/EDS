e474305ef5afd0ba9942a0179076ff3ca3155852
jonnyry <jonny.rylands@vitalsignssoftware.com>
2016-05-25 11:14:34 +0100

    EmisOpen transform

--- src/eds-transforms/src/main/java/org/endeavourhealth/transform/emis/emisopen/transforms/clinical/AllergyTransformer.java
+++ src/eds-transforms/src/main/java/org/endeavourhealth/transform/emis/emisopen/transforms/clinical/AllergyTransformer.java
@@ -1,33 +1,38 @@
 package org.endeavourhealth.transform.emis.emisopen.transforms.clinical;
 
+import org.endeavourhealth.transform.common.TransformException;
 import org.endeavourhealth.transform.emis.emisopen.schema.eommedicalrecord38.AllergyType;
 import org.endeavourhealth.transform.emis.emisopen.schema.eommedicalrecord38.MedicalRecordType;
+import org.endeavourhealth.transform.emis.emisopen.transforms.common.DateConverter;
 import org.endeavourhealth.transform.fhir.FhirUris;
-import org.hl7.fhir.instance.model.AllergyIntolerance;
-import org.hl7.fhir.instance.model.Meta;
-import org.hl7.fhir.instance.model.Resource;
+import org.endeavourhealth.transform.fhir.ReferenceHelper;
+import org.hl7.fhir.instance.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AllergyTransformer
 {
-    public static List<Resource> transform(MedicalRecordType medicalRecordType)
+    public static List<Resource> transform(MedicalRecordType medicalRecordType) throws TransformException
     {
         List<Resource> resource = new ArrayList<>();
 
         for (AllergyType allergyType : medicalRecordType.getAllergyList().getAllergy())
-            resource.add(transform(allergyType));
+            resource.add(transform(allergyType, medicalRecordType.getRegistration().getGUID()));
 
         return resource;
     }
 
-    private static AllergyIntolerance transform(AllergyType allergyType)
+    private static AllergyIntolerance transform(AllergyType allergyType, String patientUuid) throws TransformException
     {
         AllergyIntolerance allergy = new AllergyIntolerance();
         allergy.setId(allergyType.getGUID());
         allergy.setMeta(new Meta().addProfile(FhirUris.PROFILE_URI_ALLERGY_INTOLERANCE));
 
+        allergy.setPatient(ReferenceHelper.createReference(ResourceType.Patient, patientUuid));
+        allergy.setRecorder(ReferenceHelper.createReference(ResourceType.Practitioner, allergyType.getOriginalAuthor().getUser().getGUID()));
+
+        allergy.setOnsetElement(DateConverter.convertPartialDateToDateTimeType(allergyType.getAssignedDate(), allergyType.getAssignedTime(), allergyType.getDatePart()));
 
 
 
--- src/eds-transforms/src/main/java/org/endeavourhealth/transform/emis/emisopen/transforms/common/DateConverter.java
+++ src/eds-transforms/src/main/java/org/endeavourhealth/transform/emis/emisopen/transforms/common/DateConverter.java
@@ -1,6 +1,12 @@
 package org.endeavourhealth.transform.emis.emisopen.transforms.common;
 
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.lang3.StringUtils;
 import org.endeavourhealth.transform.common.TransformException;
+import org.endeavourhealth.transform.emis.openhr.schema.DtDatePart;
+import org.endeavourhealth.transform.emis.openhr.schema.VocDatePart;
+import org.hl7.fhir.instance.model.DateTimeType;
+import org.hl7.fhir.instance.model.TemporalPrecisionEnum;
 
 import java.sql.Time;
 import java.text.ParseException;
@@ -46,6 +52,24 @@ public class DateConverter
         catch (ParseException e)
         {
             throw new TransformException("Could not parse time", e);
+        }
+    }
+
+    public static DateTimeType convertPartialDateToDateTimeType(String dateString, String timeString, short datePart) throws TransformException
+    {
+        switch (datePart)
+        {
+            case 0:
+            {
+                if (StringUtils.isNotBlank(timeString))
+                    return new DateTimeType(getDateAndTime(dateString, timeString), TemporalPrecisionEnum.SECOND);
+                else
+                    return new DateTimeType(getDateAndTime(dateString, timeString), TemporalPrecisionEnum.DAY);
+            }
+            case 1: return new DateTimeType(getDate("01/" + dateString), TemporalPrecisionEnum.MONTH);
+            case 2: return new DateTimeType(getDate("01/01/" + dateString), TemporalPrecisionEnum.YEAR);
+            case 3: return null;
+            default: throw new NotImplementedException("Date part not recognised");
         }
     }
 
