770e1eaaf416636ac695bee4e56bec207e3342dd
ErgoSoftUK <ergosoftuk@gmail.com>
2016-06-10 16:18:14 +0100

    Added missing PGPDecrypt config file

---
+++ src/eds-messaging-core/src/main/java/org/endeavourhealth/core/configuration/PGPDecryptConfig.java
@@ -0,0 +1,32 @@
+
+package org.endeavourhealth.core.configuration;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for PGPDecryptConfig complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="PGPDecryptConfig">
+ *   &lt;complexContent>
+ *     &lt;extension base="{}ComponentConfig">
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "PGPDecryptConfig")
+public class PGPDecryptConfig
+    extends ComponentConfig
+{
+
+
+}
