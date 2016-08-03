1f8d6beb92fd989d2dbdda02896458de90ee4ca8
Ben Spencer <ben.spencer@healthforge.io>
2016-08-03 14:56:51 +0100

    SFTP reader: allow specifying config file location as system property.

--- src/eds-sftpreader/src/main/java/org/endeavourhealth/sftpreader/Configuration.java
+++ src/eds-sftpreader/src/main/java/org/endeavourhealth/sftpreader/Configuration.java
@@ -41,8 +41,14 @@ public final class Configuration
 
     private void loadLocalConfiguration() throws Exception
     {
-        LOG.info("Loading local configuration file from resource " + CONFIG_RESOURCE);
-        localConfiguration = XmlSerializer.deserializeFromResource(SftpReaderConfiguration.class, CONFIG_RESOURCE, CONFIG_XSD);
+        String path = System.getProperty("sftpreader.configurationFile");
+        if (path != null) {
+            LOG.info("Loading local configuration file from path " + path);
+            localConfiguration = XmlSerializer.deserializeFromFile(SftpReaderConfiguration.class, path, CONFIG_XSD);
+        } else {
+            LOG.info("Loading local configuration file from resource " + CONFIG_RESOURCE);
+            localConfiguration = XmlSerializer.deserializeFromResource(SftpReaderConfiguration.class, CONFIG_RESOURCE, CONFIG_XSD);
+        }
     }
 
     private void loadDbConfiguration() throws PgStoredProcException, SQLException
