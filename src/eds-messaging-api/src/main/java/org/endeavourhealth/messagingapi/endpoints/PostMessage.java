6c994d0d5e60db2557f72e0a782287bdc61f2332
Drew Littler <drewlittler@hotmail.com>
2016-08-10 16:44:28 +0100

    Fixed logback.xml to use environment variables

--- src/eds-messaging-api/src/main/java/org/endeavourhealth/messagingapi/endpoints/PostMessage.java
+++ src/eds-messaging-api/src/main/java/org/endeavourhealth/messagingapi/endpoints/PostMessage.java
@@ -3,7 +3,10 @@ package org.endeavourhealth.messagingapi.endpoints;
 import org.endeavourhealth.core.configuration.Pipeline;
 import org.endeavourhealth.messagingapi.configuration.ConfigManager;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
