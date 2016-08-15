9124e2d4a1390eb3f7de9b76c99af1f4e1d6cb1c
Mark Sinclair <mark.sinclair@healthforge.io>
2016-08-15 11:25:54 +0100

    Messaging API has some auth and authz protection as eds-ui now

--- src/eds-messaging-api/src/main/java/org/endeavourhealth/messagingapi/endpoints/PostMessage.java
+++ src/eds-messaging-api/src/main/java/org/endeavourhealth/messagingapi/endpoints/PostMessage.java
@@ -3,6 +3,7 @@ package org.endeavourhealth.messagingapi.endpoints;
 import org.endeavourhealth.core.configuration.Pipeline;
 import org.endeavourhealth.messagingapi.configuration.ConfigManager;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -19,6 +20,7 @@ public class PostMessage extends AbstractEndpoint {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/PostMessage")
+	@RolesAllowed({"eds_messaging_post"})
 	public Response postMessage(@Context HttpHeaders headers, String body) {
 		Pipeline pipeline = ConfigManager.getInstance().getPostMessage().getPipeline();
 		return Process(headers, body, pipeline);
@@ -28,6 +30,7 @@ public class PostMessage extends AbstractEndpoint {
 	// @Consumes(MediaType.APPLICATION_JSON)
 	// @Produces(MediaType.APPLICATION_JSON)
 	@Path("/PostMessageAsync")
+	@RolesAllowed({"eds_messaging_post"})
 	public Response postMessageAsync(@Context HttpHeaders headers, String body) {
 		Pipeline pipeline = ConfigManager.getInstance().getPostMessageAsync().getPipeline();
 		return Process(headers, body, pipeline);
