62f1376ef2c55dd159f3462f2f0b5c718a13e5e4
ErgoSoftUK <ergosoftuk@gmail.com>
2016-08-31 13:56:37 +0100

    Added message viewer window (double-click)

--- src/eds-subscriber/src/main/java/Subscriber.java
+++ src/eds-subscriber/src/main/java/Subscriber.java
@@ -1,27 +1,56 @@
 import com.sun.net.httpserver.HttpServer;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Subscriber {
+	private static JFrame app;
+
 	private JPanel main;
 	private JList list1;
 	private JLabel lblStatus;
 	private DefaultListModel<String> listModel = new DefaultListModel<>();
 
 	public static void main(String[] args) throws IOException {
-		JFrame frame = new JFrame("Subscriber");
-		frame.setContentPane(new Subscriber().main);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
+		app = new JFrame("Subscriber");
+		app.setContentPane(new Subscriber().main);
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.pack();
+		app.setVisible(true);
 	}
 
 	public Subscriber() throws IOException {
 		list1.setModel(listModel);
 		initializeHttpListener();
 		lblStatus.setText("Listening...");
+		list1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				super.mouseClicked(e);
+				if (e.getClickCount() == 2) {
+					int index = list1.locationToIndex(e.getPoint());
+					if (index >= 0) {
+						displayMessage(index);
+					}
+				}
+			}
+		});
+	}
+
+	private void displayMessage(int index) {
+		String message = listModel.get(index);
+		MessageBody messageBodyForm = new MessageBody();
+		messageBodyForm.setText(message);
+
+		final JDialog frame = new JDialog(app, "Message Body", true);
+		frame.setMaximumSize(new Dimension(1280,1024));
+		frame.setContentPane(messageBodyForm.main);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	private void initializeHttpListener() throws IOException {
