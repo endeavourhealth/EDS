6ddea46f106c7618b4586d7c90cb9691cd738e38
ErgoSoftUK <ergosoftuk@gmail.com>
2016-09-02 15:42:08 +0100

    Changed hibernate to use override environment variables and moved persistence file to correct location

--- src/eds-ui/src/main/java/org/endeavourhealth/ui/database/PersistenceManager.java
+++ src/eds-ui/src/main/java/org/endeavourhealth/ui/database/PersistenceManager.java
@@ -4,14 +4,25 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.HashMap;
+import java.util.Map;
 
 public enum PersistenceManager {
     INSTANCE;
+    private static final String JDBC_URL_ENV_VAR = "LOGBACK_JDBC_URL";
+    private static final String JDBC_USER_ENV_VAR = "LOGBACK_JDBC_USERNAME";
+    private static final String JDBC_PASSWORD_ENV_VAR = "LOGBACK_JDBC_PASSWORD";
 
     private EntityManagerFactory emFactory;
 
     private PersistenceManager() {
-        emFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit", new HashMap());
+        Map<String, String> envVars = System.getenv();
+        Map<String, Object> override = new HashMap<>();
+
+        if (envVars.containsKey(JDBC_URL_ENV_VAR)) override.put("hibernate.connection.url", envVars.get(JDBC_URL_ENV_VAR));
+        if (envVars.containsKey(JDBC_USER_ENV_VAR)) override.put("hibernate.connection.username", envVars.get(JDBC_USER_ENV_VAR));
+        if (envVars.containsKey(JDBC_PASSWORD_ENV_VAR)) override.put("hibernate.connection.password", envVars.get(JDBC_PASSWORD_ENV_VAR));
+
+        emFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit", override);
     }
 
     public EntityManager getEntityManager() {
