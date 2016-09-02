d2979ce862c97dfda7cb3ed634dba895eec43fd5
ErgoSoftUK <ergosoftuk@gmail.com>
2016-09-02 16:25:34 +0100

    Rework of persistence manager

--- src/eds-ui/src/main/java/org/endeavourhealth/ui/database/PersistenceManager.java
+++ src/eds-ui/src/main/java/org/endeavourhealth/ui/database/PersistenceManager.java
@@ -1,5 +1,8 @@
 package org.endeavourhealth.ui.database;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -11,25 +14,39 @@ public enum PersistenceManager {
     private static final String JDBC_URL_ENV_VAR = "LOGBACK_JDBC_URL";
     private static final String JDBC_USER_ENV_VAR = "LOGBACK_JDBC_USERNAME";
     private static final String JDBC_PASSWORD_ENV_VAR = "LOGBACK_JDBC_PASSWORD";
+    private static final Logger LOG = LoggerFactory.getLogger(PersistenceManager.class);
 
     private EntityManagerFactory emFactory;
 
-    private PersistenceManager() {
-        Map<String, String> envVars = System.getenv();
-        Map<String, Object> override = new HashMap<>();
-
-        if (envVars.containsKey(JDBC_URL_ENV_VAR)) override.put("hibernate.connection.url", envVars.get(JDBC_URL_ENV_VAR));
-        if (envVars.containsKey(JDBC_USER_ENV_VAR)) override.put("hibernate.connection.username", envVars.get(JDBC_USER_ENV_VAR));
-        if (envVars.containsKey(JDBC_PASSWORD_ENV_VAR)) override.put("hibernate.connection.password", envVars.get(JDBC_PASSWORD_ENV_VAR));
-
-        emFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit", override);
-    }
 
     public EntityManager getEntityManager() {
-        return emFactory.createEntityManager();
+        return getEmFactory().createEntityManager();
     }
 
     public void close() {
-        emFactory.close();
+        getEmFactory().close();
+    }
+
+    private synchronized EntityManagerFactory getEmFactory() {
+        if (emFactory == null) {
+            try {
+                Map<String, String> envVars = System.getenv();
+                Map<String, Object> override = new HashMap<>();
+
+                if (envVars.containsKey(JDBC_URL_ENV_VAR))
+                    override.put("hibernate.connection.url", envVars.get(JDBC_URL_ENV_VAR));
+                if (envVars.containsKey(JDBC_USER_ENV_VAR))
+                    override.put("hibernate.connection.username", envVars.get(JDBC_USER_ENV_VAR));
+                if (envVars.containsKey(JDBC_PASSWORD_ENV_VAR))
+                    override.put("hibernate.connection.password", envVars.get(JDBC_PASSWORD_ENV_VAR));
+
+                emFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit", override);
+            }
+            catch (Exception e) {
+                LOG.error("Error initializing persistence manager : " + e.getMessage());
+            }
+        }
+
+        return emFactory;
     }
 }
