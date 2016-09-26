46713e6730538b2520338df3b926718a3b069268
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2016-09-26 10:33:58 +0100

    Record viewer

--- src/eds-ui/src/main/java/org/endeavourhealth/ui/business/recordViewer/RecordViewerBusiness.java
+++ src/eds-ui/src/main/java/org/endeavourhealth/ui/business/recordViewer/RecordViewerBusiness.java
@@ -1,10 +1,15 @@
 package org.endeavourhealth.ui.business.recordViewer;
 
+import org.endeavourhealth.core.data.ehr.ResourceRepository;
+import org.endeavourhealth.core.data.ehr.models.ResourceByPatient;
 import org.endeavourhealth.ui.business.recordViewer.models.JsonPatient;
+import org.hl7.fhir.instance.formats.JsonParser;
 import org.hl7.fhir.instance.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public final class RecordViewerBusiness {
@@ -13,11 +18,37 @@ public final class RecordViewerBusiness {
 
     public static JsonPatient getDemographics(UUID serviceId, UUID systemId, UUID patientId) throws Exception {
 
-        Patient patient = RecordViewerData.getPatientResource(serviceId, systemId, patientId);
+        Patient patient = getSingleResource(serviceId, systemId, patientId, Patient.class);
 
         return PatientTransform.transform(patient)
             .setServiceId(serviceId)
             .setSystemId(systemId)
             .setPatientId(patientId);
+    }
+
+    private static <T> T getSingleResource(UUID serviceId, UUID systemId, UUID patientId, Class<T> resourceType) throws Exception {
+        List<T> resources = getResource(serviceId, systemId, patientId, resourceType);
+
+        if ((resources == null) || (resources.size() == 0))
+            throw new Exception(resourceType + " resource not found");
+
+        if (resources.size() > 1)
+            throw new Exception("Multiple " + resourceType + " resources found");
+
+        return resources.get(0);
+    }
+
+    private static <T> List<T> getResource(UUID serviceId, UUID systemId, UUID patientId, Class<T> resourceType) throws Exception {
+        ResourceRepository resourceRepository = new ResourceRepository();
+        List<ResourceByPatient> resourceByPatientList = resourceRepository.getResourcesByPatient(serviceId, systemId, patientId, resourceType.getSimpleName());
+
+        JsonParser jsonParser = new JsonParser();
+
+        List<T> result = new ArrayList<>();
+
+        for (ResourceByPatient resourceByPatient : resourceByPatientList)
+            result.add((T)jsonParser.parse(resourceByPatient.getResourceData()));
+
+        return result;
     }
 }
