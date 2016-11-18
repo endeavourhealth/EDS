98b44bcd062323112e6c8782bd97062fc55c2db6
ErgoSoftUK <ergosoftuk@gmail.com>
2016-11-18 18:22:10 +0000

    eds-ui ng2 - "Transform Errors" fix

--- src/eds-ui/src/main/frontend/app/transformErrors/transformErrors.module.ts
+++ src/eds-ui/src/main/frontend/app/transformErrors/transformErrors.module.ts
@@ -3,11 +3,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {TransformErrorsComponent} from "./transformErrors.component";
 import {TransformErrorsService} from "./transformErrors.service";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
 	imports : [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		NgbModule
 	],
 	declarations : [
 		TransformErrorsComponent
