50830fc1f04ba958f4367387f661aa0bb0271f50
Jonny Rylands <jonny.rylands@vitalsignssoftware.com>
2017-02-14 17:16:41 +0000

    HL7 receiver - grant DB script

--- src/database/sql/hl7receiver/Schema/grant_schema.sql
+++ src/database/sql/hl7receiver/Schema/grant_schema.sql
@@ -18,3 +18,8 @@ grant all on schema dictionary TO postgres;
 grant all privileges on all tables in schema dictionary to postgres;
 grant all privileges on all sequences in schema dictionary to postgres;
 grant all privileges on all functions in schema dictionary to postgres;
+
+grant all on schema helper TO postgres;
+grant all privileges on all tables in schema helper to postgres;
+grant all privileges on all sequences in schema helper to postgres;
+grant all privileges on all functions in schema helper to postgres;
