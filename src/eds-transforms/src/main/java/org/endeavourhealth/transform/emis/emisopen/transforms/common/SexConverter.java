f1c8c1516560bb7cefa1ce87ff43397246b5048b
Drew Littler <drewlittler@hotmail.com>
2017-02-27 18:26:26 +0000

    Gender bug fix EmisOpen->FHIR

--- src/eds-transforms/src/main/java/org/endeavourhealth/transform/emis/emisopen/transforms/common/SexConverter.java
+++ src/eds-transforms/src/main/java/org/endeavourhealth/transform/emis/emisopen/transforms/common/SexConverter.java
@@ -3,9 +3,31 @@ package org.endeavourhealth.transform.emis.emisopen.transforms.common;
 import org.endeavourhealth.transform.common.exceptions.TransformException;
 import org.hl7.fhir.instance.model.Enumerations;
 
-public class SexConverter
-{
-    public static Enumerations.AdministrativeGender convertSex(String sex) throws TransformException
+public class SexConverter {
+
+    public static Enumerations.AdministrativeGender convertSex(String sex) throws TransformException {
+        sex = sex.trim();
+        sex = sex.toUpperCase();
+
+        if (sex.equals("U")
+                || sex.equals("UNKNOWN")) { //during testing with Emis, we saw at least one patient with sex UNKNOWN and not U
+            return Enumerations.AdministrativeGender.UNKNOWN;
+
+        } else if (sex.equals("M")) {
+            return Enumerations.AdministrativeGender.MALE;
+
+        } else if (sex.equals("F")) {
+            return Enumerations.AdministrativeGender.FEMALE;
+
+        } else if (sex.equals("I")) {
+            return Enumerations.AdministrativeGender.OTHER;
+
+        } else {
+            throw new TransformException("Sex vocabulary of " + sex.toString());
+        }
+    }
+
+    /*public static Enumerations.AdministrativeGender convertSex(String sex) throws TransformException
     {
         switch ((sex + "").trim().toUpperCase())
         {
@@ -15,5 +37,5 @@ public class SexConverter
             case "I": return Enumerations.AdministrativeGender.OTHER;
             default: throw new TransformException("Sex vocabulary of " + sex.toString());
         }
-    }
+    }*/
 }
