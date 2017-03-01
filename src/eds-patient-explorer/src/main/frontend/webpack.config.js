8c6e24521587f53e35bd5acfb14c06ffaafb995b
ErgoSoftUK <ergosoftuk@gmail.com>
2017-03-01 14:21:14 +0000

    Split app components into seperate bundles

--- src/eds-patient-explorer/src/main/frontend/webpack.config.js
+++ src/eds-patient-explorer/src/main/frontend/webpack.config.js
@@ -3,37 +3,68 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
-		'shim' : 'core-js/fn/object/assign',
+		'common' : './app/common/common.module.ts',
+		'config' : './app/config/config.module.ts',
+		'security' : './app/security/security.module.ts',
+		'dialogs' : './app/dialogs/dialogs.module.ts',
+
+		'folder' : './app/folder/folder.module.ts',
+		'coding' : './app/coding/coding.module.ts',
+		'practitioner' : './app/practitioner/practitioner.module.ts',
+		'library' : './app/library/library.module.ts',
+
+		'patientIdentity' : './app/patientIdentity/patientIdentity.module.ts',
+		'countReport' : './app/countReport/countReport.module.ts',
+		'recordViewer' : './app/recordViewer/recordViewer.module.ts',
+		'resources' : './app/resources/resources.module.ts',
+		'sqlEditor' : './app/sqlEditor/sqlEditor.module.ts',
+
+		'layout' : './app/layout/layout.module.ts',
+
+		'app' : './app/patientExplorer.app.ts',
 		'vendor' : './app/vendor.ts',
-		'app' : './app/patientExplorer.app.ts'
+		'shim' : 'core-js/fn/object/assign'
 	},
 	output: {
 		filename: './[name].bundle.js',
-		path: '../webapp'
+		path: __dirname + '/../webapp'
 	},
 	resolve: {
-		extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+		extensions: ['.webpack.js', '.web.js', '.ts', '.js']
 	},
 	module: {
 		loaders: [
 			{ test: /\.ts$/, loader: 'awesome-typescript-loader' },
-			{ test: /\.html/, loader: 'raw' },
+			{ test: /\.html/, loader: 'raw-loader' },
 			{ test: /\.css$/, loader: "style-loader!css-loader" },
-			{ test: /\.less$/, loader: "style!css!less" },
-			{	test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url' },
-			{ test:  /\.(jpe?g|png|gif|svg)$/i, loader: 'file' }
+			{ test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+			{	test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' },
+			{ test:  /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' }
 		]
 	},
 	externals: {
 		"jquery": "jQuery"
 	},
 	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: [
+				'app',
+				'layout',
+				'patientIdentity', 'countReport', 'recordViewer', 'resources', 'sqlEditor',
+				'folder', 'coding', 'practitioner', 'library',
+				'dialogs', 'security', 'config', 'common',
+				'shim', 'vendor'
+			]
+		}),
+		new webpack.ContextReplacementPlugin(
+			/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+			__dirname
+		),
 		new HtmlWebpackPlugin(
 		{
 			template: 'index.ejs',
 			inject: 'body'
 		}),
-		new webpack.optimize.DedupePlugin(),
 		new webpack.ProvidePlugin({
 			'moment': 'moment'
 		}),
@@ -44,9 +75,6 @@ module.exports = {
 	devServer: {
 		inline: true,
 		contentBase: '..\\webapp',
-		watch: true,
-		progress: true,
-		colors: true,
 
 		proxy: {
 			'/api': { target: 'http://localhost:8000' },
--- src/eds-ui/src/main/frontend/webpack.config.js
+++ src/eds-ui/src/main/frontend/webpack.config.js
@@ -4,31 +4,102 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: {
-		'shim' : 'core-js/fn/object/assign',
+		// Base
+		'config' : './app/config/config.module.ts',
+		'common' : './app/common/common.module.ts',
+
+		// Services
+		'admin' : './app/administration/admin.module.ts',
+		'security' : './app/security/security.module.ts',
+
+		// Components
+		'pipes' : './app/pipes/pipes.module.ts',
+		'flowchart' : './app/flowchart/flowchart.module.ts',
+		'folder' : './app/folder/folder.module.ts',
+		'mouseCapture' : './app/mouseCapture/mouseCapture.module.ts',
+
+		// Dialogs
+		'dialogs' : './app/dialogs/dialogs.module.ts',
+		'coding' : './app/coding/coding.module.ts',
+		'codeSet' : './app/codeSet/codeSet.module.ts',
+		'countReport' : './app/countReport/countReport.module.ts',
+		'dataSet' : './app/dataSet/dataSet.module.ts',
+		'expressions' : './app/expressions/expressions.module.ts',
+		'protocol' : './app/protocol/protocol.module.ts',
+		'query' : './app/query/query.module.ts',
+		'system' : './app/system/system.module.ts',
+		'tests' : './app/tests/tests.module.ts',
+
+		// Modules
+		'audit' : './app/audit/audit.module.ts',
+		'dashboard' : './app/dashboard/dashboard.module.ts',
+		'exchangeAudit' : './app/exchangeAudit/exchangeAudit.module.ts',
+		'library' : './app/library/library.module.ts',
+		'logging' : './app/logging/logging.module.ts',
+		'organisations' : './app/organisations/organisations.module.ts',
+		'queueing' : './app/queueing/queueing.module.ts',
+		'services' : './app/services/services.module.ts',
+		'stats' : './app/stats/stats.module.ts',
+		'transformErrors' : './app/transformErrors/transformErrors.module.ts',
+		'users' : './app/users/user.module.ts',
+
+		// Application
+		'layout' : './app/layout/layout.module.ts',
+		'app' : './app/dataServiceManager.app.ts',
+
+		// 3rd Party
 		'vendor' : './app/vendor.ts',
-		'app' : './app/dataServiceManager.app.ts'
+		'shim' : 'core-js/fn/object/assign'
 	},
 	output: {
 		filename: './[name].bundle.js',
-		path: '../webapp'
+		path: __dirname + '/../webapp'
 	},
 	resolve: {
-		extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+		extensions: ['.webpack.js', '.web.js', '.ts', '.js']
 	},
 	module: {
 		loaders: [
 			{ test: /\.ts$/, loader: 'awesome-typescript-loader' },
-			{ test: /\.html/, loader: 'raw' },
+			{ test: /\.html/, loader: 'raw-loader' },
 			{ test: /\.css$/, loader: "style-loader!css-loader" },
-			{ test: /\.less$/, loader: "style!css!less" },
-			{	test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url' },
-			{ test:  /\.(jpe?g|png|gif|svg)$/i, loader: 'file' }
+			{ test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+			{	test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, loader: 'url-loader' },
+			{ test:  /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' }
 		]
 	},
 	externals: {
 		"jquery": "jQuery"
 	},
 	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: [
+				// Application
+				'app', 'layout',
+
+				// Modules
+				'users', 'transformErrors', 'stats', 'services', 'queueing', 'organisations', 'logging', 'library', 'exchangeAudit', 'dashboard', 'audit',
+
+				// Dialogs
+				'tests', 'system', 'query', 'protocol', 'expressions', 'dataSet', 'countReport', 'codeSet', 'coding', 'dialogs',
+
+				// Components
+				'mouseCapture', 'folder', 'flowchart', 'pipes',
+
+				// Services
+				'security', 'admin',
+
+				// Base
+				'common', 'config',
+
+				// 3rd Party
+				'shim', 'vendor'
+			]
+		}),
+		new webpack.ContextReplacementPlugin(
+			/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+			__dirname
+		),
 		new HtmlWebpackPlugin(
 		{
 			template: 'index.ejs',
@@ -38,7 +109,6 @@ module.exports = {
 			{ from: './api-docs', to: './api-docs' },
 			{ from: './node_modules/swagger-ui/dist', to: './api-docs/swagger-ui' }
 		]),
-		new webpack.optimize.DedupePlugin(),
 		new webpack.ProvidePlugin({
 			'moment': 'moment'
 		})
@@ -46,9 +116,6 @@ module.exports = {
 	devServer: {
 		inline: true,
 		contentBase: '..\\webapp',
-		watch: true,
-		progress: true,
-		colors: true,
 
 		proxy: {
 			'/api': { target: 'http://localhost:8000' },
