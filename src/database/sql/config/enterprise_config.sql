ee5268e5a65ff989e86a0d5394a5c7ab28133002
ergosoftuk <qPellHGfNE4FpAnd0D0a>
2017-04-13 11:10:09 +0100

    Added enterprise admin db config

--- src/database/sql/config/enterprise_config.sql
+++ src/database/sql/config/enterprise_config.sql
@@ -11,6 +11,18 @@ VALUES
 	"pseudonymised": false
 }' );
 
+INSERT INTO config
+(app_id, config_id, config_data)
+VALUES
+('enterprise', 'admin_database', '{
+  "driverClass" : "org.postgresql.Driver",
+	"username" : "postgres",
+	"password" : "",
+	"url"	: "jdbc:postgresql://localhost:5432/enterprise_admin",
+	"pseudonymised": false
+}' );
+
+
 --to use mySQL
 INSERT INTO config
 (app_id, config_id, config_data)
@@ -20,6 +32,17 @@ VALUES
 	"username" : "root",
 	"password" : "",
 	"url"	: "jdbc:mysql://localhost:3306/enterprise_data?useSSL=false",
+	"pseudonymised": false
+}' );
+
+INSERT INTO config
+(app_id, config_id, config_data)
+VALUES
+('enterprise', 'admin_database', '{
+  "driverClass" : "com.mysql.cj.jdbc.Driver",
+	"username" : "root",
+	"password" : "",
+	"url"	: "jdbc:mysql://localhost:3306/enterprise_admin?useSSL=false",
 	"pseudonymised": false
 }' );
 
