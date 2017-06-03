8338d9455f0fd3651da54be9fe1eddc3dcefd952
Richard Collier <ergosoftuk@gmail.com>
2017-06-03 15:33:56 +0100

    Extracted cassandra, eds-core and eds-transforms into own modules Migrated to split components (rather than common)

--- src/cassandra/src/main/java/org/endeavourhealth/common/cassandra/RepositoryException.java
+++
@@ -1,7 +0,0 @@
-package org.endeavourhealth.common.cassandra;
-
-public class RepositoryException extends Exception {
-    public RepositoryException(String message) { super(message); }
-    public RepositoryException(String message, Throwable cause) { super(message, cause); }
-    public RepositoryException(Throwable cause) { super(cause); }
-}
--- src/eds-transforms/src/main/java/org/endeavourhealth/transform/ui/models/resources/admin/UIAppointment.java
+++
@@ -1,9 +0,0 @@
-package org.endeavourhealth.transform.ui.models.resources.admin;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import org.endeavourhealth.transform.ui.models.resources.UIResource;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class UIAppointment extends UIResource<UIAppointment> {
-
-}
