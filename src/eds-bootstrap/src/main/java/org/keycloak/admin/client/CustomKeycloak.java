014d276b765ec7c22420bc4b1e5a9d8181536776
Richard Collier <ergosoftuk@gmail.com>
2017-06-05 17:00:23 +0100

    Extracted keycloak modules to their own repo

--- src/eds-bootstrap/src/main/java/org/keycloak/admin/client/CustomKeycloak.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.admin.client;
-
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-
-public class CustomKeycloak extends Keycloak {
-    public CustomKeycloak(String serverUrl, String realm, String username, String password, String clientId, String clientSecret, String grantType, ResteasyClient resteasyClient) {
-        super(serverUrl, realm, username, password, clientId, clientSecret, grantType, resteasyClient);
-    }
-}
