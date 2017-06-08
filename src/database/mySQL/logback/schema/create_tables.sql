1680ca6deeaf160d7a3e432533da76693746545e
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2017-06-08 09:26:50 +0100

    Minor cosmetics

--- src/database/mySQL/logback/schema/create_tables.sql
+++ src/database/mySQL/logback/schema/create_tables.sql
@@ -4,37 +4,37 @@ DROP TABLE IF EXISTS logback.logging_event;
 
 CREATE TABLE logback.logging_event
   (
-    timestmp          BIGINT NOT NULL,
-    formatted_message TEXT NOT NULL,
-    logger_name       VARCHAR(254) NOT NULL,
-    level_string      VARCHAR(254) NOT NULL,
-    thread_name       VARCHAR(254),
-    reference_flag    SMALLINT,
-    arg0              VARCHAR(254),
-    arg1              VARCHAR(254),
-    arg2              VARCHAR(254),
-    arg3              VARCHAR(254),
-    caller_filename   VARCHAR(254) NOT NULL,
-    caller_class      VARCHAR(254) NOT NULL,
-    caller_method     VARCHAR(254) NOT NULL,
-    caller_line       CHAR(4) NOT NULL,
-    event_id          BIGINT auto_increment PRIMARY KEY
+    timestmp          bigint NOT NULL,
+    formatted_message text NOT NULL,
+    logger_name       varchar(254) NOT NULL,
+    level_string      varchar(254) NOT NULL,
+    thread_name       varchar(254),
+    reference_flag    smallint,
+    arg0              varchar(254),
+    arg1              varchar(254),
+    arg2              varchar(254),
+    arg3              varchar(254),
+    caller_filename   varchar(254) NOT NULL,
+    caller_class      varchar(254) NOT NULL,
+    caller_method     varchar(254) NOT NULL,
+    caller_line       char(4) NOT NULL,
+    event_id          bigint auto_increment PRIMARY KEY
   );
 
 CREATE TABLE logback.logging_event_property
   (
-    event_id	        BIGINT NOT NULL,
-    mapped_key        VARCHAR(254) NOT NULL,
-    mapped_value      VARCHAR(1024),
+    event_id	        bigint NOT NULL,
+    mapped_key        varchar(254) NOT NULL,
+    mapped_value      varchar(1024),
     PRIMARY KEY(event_id, mapped_key),
     FOREIGN KEY (event_id) REFERENCES logback.logging_event(event_id)
   );
 
 CREATE TABLE logback.logging_event_exception
   (
-    event_id          BIGINT NOT NULL,
-    i                 SMALLINT NOT NULL,
-    trace_line        VARCHAR(254) NOT NULL,
+    event_id          bigint NOT NULL,
+    i                 smallint NOT NULL,
+    trace_line        varchar(254) NOT NULL,
     PRIMARY KEY(event_id, i),
     FOREIGN KEY (event_id) REFERENCES logback.logging_event(event_id)
   );
