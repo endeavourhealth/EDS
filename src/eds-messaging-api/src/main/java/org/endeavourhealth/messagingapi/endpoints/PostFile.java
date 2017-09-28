5b2cd275f55ee9701498aa022c427530b35b30a4
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2017-09-28 15:36:04 +0100

    Direct call into data sharing manager interface to check for valid organisation DPA

--- src/eds-messaging-api/src/main/java/org/endeavourhealth/messagingapi/endpoints/PostFile.java
+++ src/eds-messaging-api/src/main/java/org/endeavourhealth/messagingapi/endpoints/PostFile.java
@@ -17,14 +17,9 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.FileCleanerCleanup;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileCleaningTracker;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicHeader;
 import org.endeavourhealth.common.config.ConfigManager;
+import org.endeavourhealth.datasharingmanagermodel.models.database.DataProcessingAgreementEntity;
 
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.ServletContext;
@@ -33,26 +28,22 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 @Path("/")
 public class PostFile extends AbstractEndpoint {
-	private static String organisationId;
-
 	@POST
 	@Path("/PostFile")
 	@RolesAllowed({"tpp-bulk-extract-provider", "homerton-bulk-extract-provider"})
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public Response uploadFiles(@Context final HttpServletRequest request) {
 		// check publisher has a DPA with Discovery
-		organisationId = request.getQueryString().replaceAll("organisationId=","");
-		if (publisherHasDPA(request)) {
+		String organisationId = request.getQueryString().replaceAll("organisationId=","");
+		if (publisherHasDPA(organisationId)) {
 			// Check that we have a multi-part file upload request
 			boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 			if (isMultipart) {
@@ -71,7 +62,7 @@ public class PostFile extends AbstractEndpoint {
 				try {
 					// Parse and upload the files into AWS
 					List<FileItem> items = upload.parseRequest(request);
-					MoveDataFilesToAWS(items);
+					MoveDataFilesToAWS(organisationId, items);
 
 				} catch (Exception e) {
 					return Response.serverError().status(Response.Status.PRECONDITION_FAILED).entity(e.getMessage()).build();
@@ -83,9 +74,9 @@ public class PostFile extends AbstractEndpoint {
 		return Response.serverError().status(Response.Status.METHOD_NOT_ALLOWED).entity(String.format("No DPA for publishing organisation: %s",organisationId)).build();
 	}
 
-	private static void MoveDataFilesToAWS(List<FileItem> fileItems) throws Exception {
+	private static void MoveDataFilesToAWS(String organisationId, List<FileItem> fileItems) throws Exception {
 		{
-			JsonNode apiAWSConfig = null;
+			JsonNode apiAWSConfig;
 			try {
 				apiAWSConfig = ConfigManager.getConfigurationAsJson("aws", "messaging-api");
 			}
@@ -119,7 +110,7 @@ public class PostFile extends AbstractEndpoint {
 						metaData.setContentLength(sizeInBytes);
 
 						// create the AWS object key from the inbound filename
-						String keyPath = createFileKey(fileItem.getName());
+						String keyPath = createFileKey(organisationId, fileItem.getName());
 						System.out.println("keyPath: " + keyPath + " available received bytes: " + sizeInBytes);
 
 						// upload the file stream to AWS
@@ -160,33 +151,18 @@ public class PostFile extends AbstractEndpoint {
 		}
 	}
 
-	private static String createFileKey(String filePath) {
+	private static String createFileKey(String organisationId, String filePath) {
 		return organisationId.concat("/data/" + filePath.replace("\\", "/"));
 	}
 
-	private static boolean publisherHasDPA(HttpServletRequest request)  {
+	private static boolean publisherHasDPA(String organisationId)  {
 		// check DPA in place for data publishing org
-		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-			JsonNode dsaAppConfig = ConfigManager.getConfigurationAsJson("application","data-sharing-manager");
-			String url = dsaAppConfig.findValue("appUrl").textValue()+"/api/dpa/checkOrganisationWithCount?odsCode="+organisationId;
-			HttpGet httpGet = new HttpGet(url);
-			// authenticated already from calling client, so re-use the header
-			String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-			httpGet.addHeader(new BasicHeader(HttpHeaders.AUTHORIZATION, authHeader));
-			HttpResponse response = httpClient.execute(httpGet);
-			if (response.getStatusLine().getStatusCode() >= 200 && response.getStatusLine().getStatusCode() < 300)
-			{
-				String responseValue = "0";
-				if (response.getEntity() != null) {
-					if (response.getEntity().getContent() != null) {
-						responseValue = IOUtils.toString(response.getEntity().getContent());
-					}
-				}
-				return Integer.parseInt(responseValue)>0;
-			}
-			else
-				return false;
-		} catch (IOException e) {
+		try {
+			List<DataProcessingAgreementEntity> matchingDpa = DataProcessingAgreementEntity.getDataProcessingAgreementsForOrganisation(organisationId);
+
+			return matchingDpa.size()>0;
+		}
+		catch (Exception Ex) {
 			return false;
 		}
 	}
