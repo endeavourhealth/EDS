abe34eb0a642f1338d5261bce0da9e3f745d2948
Richard Collier <ergosoftuk@gmail.com>
2017-12-20 10:01:56 +0000

    Fix caching issue

--- src/eds-ui/src/main/frontend/webpack.config.js
+++ src/eds-ui/src/main/frontend/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
 		'shim' : 'core-js/fn/object/assign'
 	},
 	output: {
-		filename: './[name].bundle.js',
+		filename: './[name].[hash].bundle.js',
 		path: __dirname + '/../webapp'
 	},
 	resolve: {
