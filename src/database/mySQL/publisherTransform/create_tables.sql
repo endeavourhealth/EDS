f6da99908c967b250855642e20a035eecc1b149c
eaiportal <pholm@eaiportal.com>
2018-01-19 18:44:59 +0000

    New table to track patient/encounter merge and moves

--- src/database/mySQL/publisherTransform/create_tables.sql
+++ src/database/mySQL/publisherTransform/create_tables.sql
@@ -2,6 +2,7 @@ use publisher_transform_???;
 
 DROP TABLE IF EXISTS resource_id_map;
 DROP TABLE IF EXISTS sus_resource_map;
+DROP TABLE IF EXISTS resource_merge_map;
 
 CREATE TABLE resource_id_map (
 	service_id char(36),
@@ -16,8 +17,17 @@ ON resource_id_map (resource_type, eds_id);
 
 CREATE TABLE sus_resource_map (
   service_id char(36),
+	resource_type varchar(50),
   source_row_id varchar(200),
   destination_resource_type varchar(100),
   destination_resource_id char(36),
   CONSTRAINT pk_sus_resource_map PRIMARY KEY (service_id, source_row_id, destination_resource_type, destination_resource_id)
+);
+
+CREATE TABLE resource_merge_map (
+  service_id char(36),
+  source_resource_id char(36),
+  destination_resource_id char(36),
+  updated_at datetime,
+  CONSTRAINT pk_resource_merge_map PRIMARY KEY (service_id, source_resource_id)
 );
