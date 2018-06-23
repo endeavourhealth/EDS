9b9da77c94adec739f8877df39d371c3a75fe749
Drew Littler <drewlittler@hotmail.com>
2018-06-23 18:47:30 +0100

    New transform repo

--- src/database/mySQL/audit/populate_schema.sql
+++ src/database/mySQL/audit/populate_schema.sql
@@ -2,3 +2,4 @@ USE audit;
 
 INSERT INTO queued_message_type VALUES (1, 'inbound data');
 INSERT INTO queued_message_type VALUES (2, 'outbound data');
+INSERT INTO queued_message_type VALUES (3, 'resource temp store');
