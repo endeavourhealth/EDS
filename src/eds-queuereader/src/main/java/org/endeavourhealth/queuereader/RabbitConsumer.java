2437e44b47ffc671ff43f870aede4f5f5e124dd3
Drew Littler <drewlittler@hotmail.com>
2018-10-02 10:53:47 +0100

    Changed Rabbit consumer to handle duplicate concurrent requests when Rabbit has a failover

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/RabbitConsumer.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/RabbitConsumer.java
@@ -18,20 +18,22 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class RabbitConsumer extends DefaultConsumer {
 	private static final Logger LOG = LoggerFactory.getLogger(RabbitConsumer.class);
 
+	//variables to prevent duplicate processing in the event of Rabbit fail-overs
+	private static final List<RabbitConsumer_State> messagesBeingProcessed = new ArrayList<>();
+
+	//members
 	private final QueueReaderConfiguration configuration;
 	private final String configId;
 	private final RabbitHandler handler;
 	private final PipelineProcessor pipeline;
 
-	private UUID lastExchangeAttempted;
+	private RabbitConsumer_State lastExchangeAttempted;
 	private int lastExchangeAttempts;
 
 	public RabbitConsumer(Channel channel, QueueReaderConfiguration configuration, String configId, RabbitHandler handler) {
@@ -46,33 +48,166 @@ public class RabbitConsumer extends DefaultConsumer {
 		checkIfKillFileExists();
 	}
 
+
+
 	@Override
 	public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] bytes) throws IOException {
+
+		RabbitConsumer_State processingState = findProcessingState(bytes, properties.getHeaders());
+
+		//check if we should process the message or not
+		long deliveryTag = envelope.getDeliveryTag();
+		if (!processMessageNow(processingState, deliveryTag)) {
+			return;
+		}
+
+		boolean shouldAck = false;
+
+		Exchange exchange = decodeExchange(properties, bytes);
+		if (exchange != null) {
+			processingState.setExchangeId(exchange.getId());
+			LOG.info("Received " + configId + " exchange " + exchange.getId() + " from Rabbit");
+
+			// Process the message
+			if (pipeline.execute(exchange)) {
+				shouldAck = true;
+
+				//when we successfully process something, clear this
+				lastExchangeAttempted = null;
+
+			} else {
+				//keep track of rejections and send slack messages if required
+				updateAttemptsOnFailure(processingState, exchange.getException());
+			}
+		}
+
+		//update our state and send the ack or nak
+		processingState.setShouldAck(new Boolean(shouldAck));
+		ackOrNak(processingState, deliveryTag);
+
+		//tidy up and remove our processing state object
+		processingFinished(processingState);
+
+		//see if we've been told to finish
+		if (checkIfKillFileExists()) {
+			String reason = "Detected kill file";
+			stop(reason);
+		}
+	}
+
+	private static void processingFinished(RabbitConsumer_State processingState) {
+
+		int countThreadsProcessing = processingState.getProcessingCount().decrementAndGet();
+		if (countThreadsProcessing == 0) {
+			//if there are no more threads processing this delivery tag, then we just need to remove from the map
+			removeProcessingState(processingState);
+			return;
+		}
+
+		//if we have one or more threads waiting on our result, then notify them now
+		synchronized (processingState) {
+			processingState.notifyAll();
+		}
+	}
+
+
+	private boolean processMessageNow(RabbitConsumer_State processingState, long deliveryTag) throws IOException {
+
+		int countThreadsProcessing = processingState.getProcessingCount().incrementAndGet();
+
+		//if there's only one thread processing the message, then return true to let the pipeline processing happen
+		if (countThreadsProcessing == 1) {
+			return true;
+		}
+
+		//if'we already processing this delivery tag, then we've had a RabbitMQ restart or failover,
+		//in which case we don't want to just start processing the message, since it will already be in progress.
+		//Instead, we increment our wait count on the state object and then wait until the first request is complete
+		LOG.warn("Already processing message for exchange " + processingState.getExchangeId() + " so will wait until it completes - possible RabbitMQ restart or failover?");
+
+		//keep waiting until we know whether we should ack or nak
+		synchronized (processingState) {
+			while (processingState.getShouldAck() == null) {
+				try {
+					processingState.wait();
+				} catch (InterruptedException ex) {
+					LOG.error("", ex);
+				}
+			}
+		}
+
+		LOG.warn("Original processing of exchange " + processingState.getExchangeId() + " is complete, so will now ACK/NAK the re-request");
+		ackOrNak(processingState, deliveryTag);
+
+		processingFinished(processingState);
+
+		return false;
+	}
+
+	private static RabbitConsumer_State findProcessingState(byte[] body, Map<String, Object> headers) {
+
+		synchronized (messagesBeingProcessed) {
+
+			RabbitConsumer_State potentialNewState = new RabbitConsumer_State(body, headers);
+			for (RabbitConsumer_State existingState: messagesBeingProcessed) {
+				if (existingState.equals(potentialNewState)) {
+					return existingState;
+				}
+			}
+			messagesBeingProcessed.add(potentialNewState);
+			return potentialNewState;
+		}
+	}
+
+	private static void removeProcessingState(RabbitConsumer_State processingState) {
+		synchronized (messagesBeingProcessed) {
+			messagesBeingProcessed.remove(processingState);
+		}
+	}
+
+
+	private void ackOrNak(RabbitConsumer_State processingState, long deliveryTag) throws IOException {
+
+		boolean shouldAck = processingState.getShouldAck().booleanValue();
+		UUID exchangeId = processingState.getExchangeId();
+
+		if (shouldAck) {
+			//LOG.info("Successfully processed exchange {}", exchange.getExchangeId());
+			this.getChannel().basicAck(deliveryTag, false);
+			//LOG.info("Have sent ACK for exchange {}", exchange.getExchangeId());
+
+		} else {
+
+			this.getChannel().basicReject(deliveryTag, true);
+			LOG.error("Have sent REJECT for exchange " + exchangeId);
+		}
+	}
+
+	private static Exchange decodeExchange(AMQP.BasicProperties properties, byte[] bytes) throws IOException {
+
 		// Decode the message id
-		String messageId = new String(bytes, "UTF-8");
-		UUID messageUuid = UUID.fromString(messageId);
+		String exchangeId = new String(bytes, "UTF-8");
+		UUID exchangeUuid = UUID.fromString(exchangeId);
 
 		// Get the message from the db
 		String queuedMessageBody = null;
 
 		try {
 			QueuedMessageDalI queuedMessageDal = DalProvider.factoryQueuedMessageDal();
-			queuedMessageBody = queuedMessageDal.getById(messageUuid);
+			queuedMessageBody = queuedMessageDal.getById(exchangeUuid);
 		} catch (Exception ex) {
-			LOG.error("Failed to retrieve queued message " + messageId, ex);
-			this.getChannel().basicAck(envelope.getDeliveryTag(), false);
-			return;
+			LOG.error("Failed to retrieve queued message " + exchangeId, ex);
+			return null;
 		}
 
 		//seem to get brokwn messages in dev environments, so handle for now
 		if (queuedMessageBody == null) {
-			LOG.warn("Received queued message ID " + messageId + " with no actual message");
-			this.getChannel().basicAck(envelope.getDeliveryTag(), false);
-			return;
+			LOG.error("Received queued message ID " + exchangeId + " with no actual message");
+			return null;
 		}
 
 		Exchange exchange = new Exchange();
-		exchange.setId(messageUuid);
+		exchange.setId(exchangeUuid);
 		exchange.setBody(queuedMessageBody);
 		exchange.setTimestamp(new Date());
 		exchange.setHeaders(new HashMap<>());
@@ -83,50 +218,26 @@ public class RabbitConsumer extends DefaultConsumer {
 					.filter(headerKey -> headers.get(headerKey) != null)
 					.forEach(headerKey -> exchange.setHeader(headerKey, headers.get(headerKey).toString()));
 		}
-		LOG.info("Received " + configId + " exchange " + exchange.getId() + " from Rabbit");
 
-		// Process the message
-		if (pipeline.execute(exchange)) {
-			//LOG.info("Successfully processed exchange {}", exchange.getExchangeId());
-			this.getChannel().basicAck(envelope.getDeliveryTag(), false);
-			//LOG.info("Have sent ACK for exchange {}", exchange.getExchangeId());
-
-			//when we successfully process something, clear this
-			lastExchangeAttempted = null;
-
-		} else {
-			//LOG.error("Failed to process exchange {}", exchange.getExchangeId());
-			this.getChannel().basicReject(envelope.getDeliveryTag(), true);
-			LOG.error("Have sent REJECT for exchange {}", exchange.getId());
-
-			//keep track of rejections
-			updateAttemptsOnFailure(exchange);
-		}
-
-		//see if we've been told to finish
-		if (checkIfKillFileExists()) {
-			String reason = "Detected kill file";
-			stop(reason);
-		}
+		return exchange;
 	}
 
-	private void updateAttemptsOnFailure(Exchange exchange) {
-		UUID exchangeIdAttempted = exchange.getId();
+	private void updateAttemptsOnFailure(RabbitConsumer_State processingState, Exception exception) {
 
 		//if it's the same exchange ID as last time, increment the number of attempts
 		if (lastExchangeAttempted != null
-				&& lastExchangeAttempted.equals(exchangeIdAttempted)) {
+				&& lastExchangeAttempted.equals(processingState)) {
 			this.lastExchangeAttempts ++;
 
 		} else {
 			//if it's a different change to last time, reset the attempt to one
-			this.lastExchangeAttempted = exchangeIdAttempted;
+			this.lastExchangeAttempted = processingState;
 			this.lastExchangeAttempts = 1;
 
 			//send a slack message for the first failure
 			String queueName = configuration.getQueue();
-			String s = "Exchange " + exchangeIdAttempted + " rejected in " + queueName;
-			SlackHelper.sendSlackMessage(SlackHelper.Channel.QueueReaderAlerts, s, exchange.getException());
+			String s = "Exchange " + processingState.getExchangeId() + " rejected in " + queueName;
+			SlackHelper.sendSlackMessage(SlackHelper.Channel.QueueReaderAlerts, s, exception);
 		}
 
 		//if we've failed on the same exchange X times, then halt the queue reader
@@ -175,5 +286,85 @@ public class RabbitConsumer extends DefaultConsumer {
 			//LOG.trace("Kill file not found: " + killFile); //for investigation
 			return false;
 		}
+	}
+}
+
+/**
+ * storage object used to track the outcome of rabbit messages and
+ * how we responded. Purely used to get around receiving the same message
+ * twice when RabbitMQ fails over or restarts
+ */
+class RabbitConsumer_State {
+
+	private byte[] body = null;
+	private Map<String, Object> headers = null;
+	private AtomicInteger processingCount = new AtomicInteger(); //count of threads processing the same delivery tag
+	private Boolean shouldAck;
+	private UUID exchangeId;
+
+	public RabbitConsumer_State(byte[] body, Map<String, Object> headers) {
+		this.body = body;
+		this.headers = headers;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (o == this) {
+			return true;
+		}
+
+		if (!(o instanceof RabbitConsumer_State)) {
+			return false;
+		}
+
+		RabbitConsumer_State other = (RabbitConsumer_State)o;
+		if (body.length != other.body.length) {
+			return false;
+		}
+
+		for (int i=0; i<body.length; i++) {
+			if (body[i] != other.body[i]) {
+				return false;
+			}
+		}
+
+		if (headers.size() != other.headers.size()) {
+			return false;
+		}
+
+		for (String key: headers.keySet()) {
+			Object val = headers.get(key);
+			Object otherVal = other.headers.get(key);
+			if ((val == null) != (otherVal == null)) {
+				return false;
+			}
+			if (val != null
+					&& !val.equals(otherVal)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public AtomicInteger getProcessingCount() {
+		return processingCount;
+	}
+
+	public Boolean getShouldAck() {
+		return shouldAck;
+	}
+
+	public void setShouldAck(Boolean shouldAck) {
+		this.shouldAck = shouldAck;
+	}
+
+	public UUID getExchangeId() {
+		return exchangeId;
+	}
+
+	public void setExchangeId(UUID exchangeId) {
+		this.exchangeId = exchangeId;
 	}
 }
