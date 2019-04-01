acebf3e7cc40aef30939c4ca4a47db0618b9d47f
Richard Collier <ergosoftuk@gmail.com>
2019-04-01 10:46:52 +0100

    "Give details" for all options Add Company/Org and Web address

--- src/eds-messaging-api/src/main/webapp/scripts.js
+++ src/eds-messaging-api/src/main/webapp/scripts.js
@@ -192,16 +192,6 @@ function getFlag() {
 }
 
 // *************** Access request form ***************
-function reasonChanged() {
-	var reason = document.querySelector("#reason");
-	var otherGroup = document.querySelector("#otherGroup");
-
-	if (reason.value === "Other")
-		otherGroup.style.display = "block";
-	else
-		otherGroup.style.display = "none";
-}
-
 function validEmail(email) {
 	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 	return re.test(email);
