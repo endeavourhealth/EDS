68113b122c4bf49660bb7a1a580bc572ad9c420a
Froiland Go <Froiland.Go@learning-health.co.uk>
2019-04-25 14:08:25 +0100

    Renamed schedule table's comments column to name Added handling for Test Patient extension

--- src/database/mySQL/subscriber/create_tables.sql
+++ src/database/mySQL/subscriber/create_tables.sql
@@ -128,7 +128,7 @@ CREATE TABLE schedule
   start_date date,
   type character varying(255),
   location character varying(255),
-  comments varchar(150), 
+  name varchar(150), 
   CONSTRAINT pk_schedule_id PRIMARY KEY (organization_id, id),
   CONSTRAINT fk_schedule_organization_id FOREIGN KEY (organization_id)
       REFERENCES organization (id) MATCH SIMPLE
