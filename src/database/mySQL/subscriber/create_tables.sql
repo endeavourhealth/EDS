f1317c42d68374976872212e340f459516abf41d
Drew Littler <drewlittler@hotmail.com>
2019-05-02 18:27:40 +0100

    Updated subscriber DB schema

--- src/database/mySQL/subscriber/create_tables.sql
+++ src/database/mySQL/subscriber/create_tables.sql
@@ -171,7 +171,7 @@ CREATE TABLE patient
   nhs_number character varying(255),
   date_of_birth date,
   date_of_death date,
-  postcode character varying(20),
+  current_address_id bigint,
   ethnic_code_concept_id int,
   registered_practice_organization_id bigint,
   CONSTRAINT pk_patient_id_organization_id PRIMARY KEY (`organization_id`,`person_id`,`id`),
