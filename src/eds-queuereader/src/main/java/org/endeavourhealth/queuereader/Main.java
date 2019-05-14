e3e7bccc7cf64a5795decbfa78064a1bf53ecefc
Drew Littler <drewlittler@hotmail.com>
2019-05-14 16:30:06 +0100

    Force build

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -5698,6 +5698,7 @@ public class Main {
 					LOG.debug("Patient " + patientIdStr + " not found or deleted");
 				}
 
+
 				String updateSql = "UPDATE " + table + " SET done = 1 WHERE patient_id = '" + patientIdStr + "' AND service_id = '" + serviceIdStr + "';";
 				entityManager = ConnectionManager.getEdsEntityManager();
 				session = (SessionImpl)entityManager.getDelegate();
