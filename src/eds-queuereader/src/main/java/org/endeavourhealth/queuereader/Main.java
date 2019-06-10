d98c6d9f05321642bf276cf8fa955eed702cb297
Drew Littler <drewlittler@hotmail.com>
2019-06-10 12:10:59 +0100

    Fixes to Barts procedures

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -85,7 +85,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-
 	public static void main(String[] args) throws Exception {
 
 		String configId = args[0];
