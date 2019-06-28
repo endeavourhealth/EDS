4ed0bda23d323794fe00356bf137e394433c79f1
Drew Littler <drewlittler@hotmail.com>
2019-06-28 11:04:59 +0100

    Change to DDS-UI column widths

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -66,7 +66,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-
 	public static void main(String[] args) throws Exception {
 
 		String configId = args[0];
