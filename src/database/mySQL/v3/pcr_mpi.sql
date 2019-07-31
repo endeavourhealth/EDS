16db60b22f8b69c0559572d6bed2b9d886f92296
Drew Littler <drewlittler@hotmail.com>
2019-07-31 15:58:19 +0100

    New v3 schemas

--- src/database/mySQL/v3/pcr_mpi.sql
+++ src/database/mySQL/v3/pcr_mpi.sql
@@ -5,12 +5,20 @@ use pcr_mpi;
 
 DROP TABLE IF EXISTS person_record;
 DROP TABLE IF EXISTS patient_person_link;
+DROP TABLE IF EXISTS patient_person_link_history;
+DROP TABLE IF EXISTS organisation_person_routing;
+DROP TABLE IF EXISTS patient_search_local_identifier;
+DROP TABLE IF EXISTS patient_search_episode;
+DROP TABLE IF EXISTS patient_search;
+
 
 CREATE TABLE person_record
 (
   person_id int NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID for this person',
   nhs_number character(10) NOT NULL COMMENT 'NHS number for this person',
   date_of_birth date COMMENT 'DoB for this person - if DoB will not be used to match, then this should be removed',
+  publisher_pcr_database_name varchar(255) NOT NULL COMMENT 'name of the publisher pcr_patient DB that this person record is stored on',
+  gp_practice_ods_code varchar(10) COMMENT 'ODS code of the registered GP practice - not used for matching, but detecting if a person needs moving to a different DB',
   CONSTRAINT pk_person_record PRIMARY KEY (person_id)
 ) COMMENT 'defines the person ID and attributes used to match to it';
 
@@ -41,3 +49,95 @@ CREATE TABLE patient_person_link_history
 CREATE INDEX ix_updated ON patient_person_link_history (updated);
 
 
+CREATE TABLE organisation_person_routing (
+	gp_practice_ods_code varchar(10) NOT NULL COMMENT 'ODS code of a GP practice',
+    publisher_pcr_database_name varchar(255) NOT NULL COMMENT 'name of the publisher pcr_patient DB that persons registered at this pracitce should be stored on',
+    CONSTRAINT pk_organisation_person_routing PRIMARY KEY (gp_practice_ods_code)
+) COMMENT 'stores the routing of which pcr_patient database person records should be stored on';
+
+
+
+
+
+CREATE TABLE patient_search
+(
+	service_id char(36) NOT NULL,
+	nhs_number varchar(10),
+	forenames varchar(500),
+	surname varchar(500),
+	date_of_birth date,
+	date_of_death date,
+	address_line_1 VARCHAR(255),
+	address_line_2 VARCHAR(255),
+	address_line_3 VARCHAR(255),
+	city VARCHAR(255),
+	district VARCHAR(255),
+	postcode varchar(8),
+	gender varchar(7),
+	patient_id int NOT NULL,
+	last_updated timestamp NOT NULL,
+	registered_practice_ods_code VARCHAR(50),
+	dt_deleted datetime,
+	CONSTRAINT pk_patient_search PRIMARY KEY (service_id, patient_id)
+);
+
+CREATE INDEX ix_patient
+  ON patient_search (patient_id);
+
+-- duplicate of primary key (clusterd index) so removed
+/*CREATE INDEX ix_service_patient
+  ON patient_search (service_id, patient_id);*/
+
+CREATE INDEX ix_service_date_of_birth
+  ON patient_search (service_id, date_of_birth, dt_deleted);
+
+-- swap index to be NHS Number first, since that's more selective than a long list of service IDs
+/*CREATE INDEX ix_service_nhs_number
+  ON patient_search (service_id, nhs_number);*/
+
+CREATE INDEX ix_service_nhs_number_2
+  ON patient_search (nhs_number, service_id, dt_deleted);
+
+CREATE INDEX ix_service_surname_forenames
+  ON patient_search (service_id, surname, forenames, dt_deleted);
+
+
+
+CREATE TABLE patient_search_episode
+(
+	service_id char(36) NOT NULL,
+	patient_id int NOT NULL,
+	episode_id int NOT NULL,
+	registration_start date,
+	registration_end date,
+	care_mananger VARCHAR(255),
+	organisation_name VARCHAR(255),
+	organisation_type_code varchar(10),
+	registration_type_code varchar(10),
+	last_updated timestamp NOT NULL,
+	registration_status_code varchar(10),
+	dt_deleted datetime,
+	CONSTRAINT pk_patient_search_episode PRIMARY KEY (service_id, patient_id, episode_id)
+);
+
+-- unique index required so patient merges trigger a change in patient_id
+CREATE UNIQUE INDEX uix_patient_search_episode_id
+  ON patient_search_episode (episode_id);
+
+CREATE TABLE patient_search_local_identifier
+(
+	service_id char(36) NOT NULL,
+	local_id varchar(1000),
+	local_id_system varchar(1000),
+	patient_id int NOT NULL,
+	last_updated timestamp NOT NULL,
+	dt_deleted datetime,
+	CONSTRAINT pk_patient_search_local_identifier PRIMARY KEY (service_id, patient_id, local_id_system, local_id),
+	CONSTRAINT fk_patient_search_local_identifier_patient_id FOREIGN KEY (service_id, patient_id)
+		REFERENCES patient_search (service_id, patient_id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+-- index so patient search by local ID works in timely fashion
+CREATE INDEX ix_patient_search_local_identifier_id_service_patient
+  ON patient_search_local_identifier (local_id, service_id, patient_id, dt_deleted);
--- src/database/mySQL/v3/pcr_subscriber.sql
+++ src/database/mySQL/v3/pcr_subscriber.sql
@@ -10,19 +10,19 @@ DROP TABLE IF EXISTS subscriber_pseudo_id_map;
 drop trigger if exists after_subscriber_feed_update;
 
 create table subscriber_feed_state (
-	publisher_pcr_database_name varchar(255) NOT NULL COMMENT ''name of the publisher PCR DB, of which there will be multiple if a subscriber needs to source from multiple sources'',
-    last_run_date datetime(3) NOT NULL COMMENT ''last time this subscriber feed was refreshed from the publisher'',
-    last_event_id int NOT NULL COMMENT ''last event ID from the event_log table in the publisher PCR DB'',
+	publisher_pcr_database_name varchar(255) NOT NULL COMMENT 'name of the publisher PCR DB, of which there will be multiple if a subscriber needs to source from multiple sources',
+    last_run_date datetime(3) NOT NULL COMMENT 'last time this subscriber feed was refreshed from the publisher',
+    last_event_id int NOT NULL COMMENT 'last event ID from the event_log table in the publisher PCR DB',
     CONSTRAINT pk_subscriber_feed_state PRIMARY KEY (publisher_pcr_database_name)
-) COMMENT ''table to store the last publisher event ID transformed/processed for this subscriber'';
+) COMMENT 'table to store the last publisher event ID transformed/processed for this subscriber';
 
 create table subscriber_feed_history (
-	inserted_at datetime(3) NOT NULL COMMENT ''when this record was inserted into the history'',
-	publisher_pcr_database_name varchar(255) NOT NULL COMMENT ''name of the publisher PCR DB, of which there will be multiple if a subscriber needs to source from multiple sources'',
-    last_run_date datetime(3) NOT NULL COMMENT ''last time this subscriber feed was refreshed from the publisher'',
-    last_event_id int NOT NULL COMMENT ''last event ID from the event_log table in the publisher PCR DB'',
+	inserted_at datetime(3) NOT NULL COMMENT 'when this record was inserted into the history',
+	publisher_pcr_database_name varchar(255) NOT NULL COMMENT 'name of the publisher PCR DB, of which there will be multiple if a subscriber needs to source from multiple sources',
+    last_run_date datetime(3) NOT NULL COMMENT 'last time this subscriber feed was refreshed from the publisher',
+    last_event_id int NOT NULL COMMENT 'last event ID from the event_log table in the publisher PCR DB',
     CONSTRAINT pk_subscriber_feed_history PRIMARY KEY (publisher_pcr_database_name, inserted_at)
-) COMMENT ''table to store the history of when events from publisher DBs were applied to this subscriber'';
+) COMMENT 'table to store the history of when events from publisher DBs were applied to this subscriber';
 
 -- use trigger to populate history when state is updated
 DELIMITER $$
@@ -48,12 +48,12 @@ DELIMITER ;
 
 CREATE TABLE subscriber_id_map
 (
-  subscriber_table tinyint NOT NULL COMMENT ''ID of the target table this ID is for'',
-  subscriber_id bigint NOT NULL COMMENT ''unique ID allocated for the subscriber DB'',
-  source_id varchar(250) NOT NULL COMMENT ''Source ID from the PCR table this this is mapped from, typically a composite of table name/ID and record ID'',
-  dt_previously_sent datetime NULL COMMENT ''the date time of the previously sent version of this resource (or null if deleted)'',
+  subscriber_table tinyint NOT NULL COMMENT 'ID of the target table this ID is for',
+  subscriber_id bigint NOT NULL COMMENT 'unique ID allocated for the subscriber DB',
+  source_id varchar(250) NOT NULL COMMENT 'Source ID from the PCR table this this is mapped from, typically a composite of table name/ID and record ID',
+  dt_previously_sent datetime NULL COMMENT 'the date time of the previously sent version of this resource (or null if deleted)',
   CONSTRAINT pk_subscriber_id_map PRIMARY KEY (source_id, subscriber_table)
-) COMMENT ''table to maintain mapping of PCR records and IDs to subscriber records and IDs'';
+) COMMENT 'table to maintain mapping of PCR records and IDs to subscriber records and IDs';
 
 -- this unique index is required to make the column auto-increment
 CREATE UNIQUE INDEX uix_subscriber_id_map_auto_increment
@@ -66,10 +66,10 @@ CREATE TABLE subscriber_pseudo_id_map
 (
   patient_id int NOT NULL,
   subscriber_patient_id bigint NOT NULL,
-  salt_key_name varchar(255) NOT NULL COMMENT ''possibly needs changing to "project name" or similar?'',
+  salt_key_name varchar(255) NOT NULL COMMENT 'possibly needs changing to "project name" or similar?',
   pseudo_id varchar(255) NOT NULL,
   CONSTRAINT pk_subscriber_pseudo_id_map PRIMARY KEY (patient_id, subscriber_patient_id, salt_key_name)
-) COMMENT ''table to store any pseudo IDs generated for the subscriber so its possible to perform a backwards search to find source patient ID'';
+) COMMENT 'table to store any pseudo IDs generated for the subscriber so its possible to perform a backwards search to find source patient ID';
 
 create index ix_subscriber_pseudo_id_map_pseudo_id on subscriber_pseudo_id_map(salt_key_name, pseudo_id);
 
