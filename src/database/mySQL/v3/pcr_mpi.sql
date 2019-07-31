fa81e718b3e5a1bcc8ffae9eaf0459b05e1eb4ce
Drew Littler <drewlittler@hotmail.com>
2019-07-31 14:52:01 +0100

    New v3 schemas

--- src/database/mySQL/v3/pcr_mpi.sql
+++ src/database/mySQL/v3/pcr_mpi.sql
@@ -8,22 +8,22 @@ DROP TABLE IF EXISTS patient_person_link;
 
 CREATE TABLE person_record
 (
-  person_id int NOT NULL AUTO_INCREMENT COMMENT ''auto-generated ID for this person'',
-  nhs_number character(10) NOT NULL COMMENT ''NHS number for this person'',
-  date_of_birth date COMMENT ''DoB for this person - if DoB will not be used to match, then this should be removed'',
+  person_id int NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID for this person',
+  nhs_number character(10) NOT NULL COMMENT 'NHS number for this person',
+  date_of_birth date COMMENT 'DoB for this person - if DoB will not be used to match, then this should be removed',
   CONSTRAINT pk_person_record PRIMARY KEY (person_id)
-) COMMENT ''defines the person ID and attributes used to match to it'';
+) COMMENT 'defines the person ID and attributes used to match to it';
 
 CREATE UNIQUE INDEX ix_nhs_number ON person_record (nhs_number);
 
 
 CREATE TABLE patient_person_link
 (
-  patient_id int NOT NULL COMMENT ''refers to patient.id in pcr_patient databases'',
-  service_id character(36) NOT NULL COMMENT ''refers to admin.service table'',
-  person_id int NOT NULL COMMENT ''refers to person_record table'',
+  patient_id int NOT NULL COMMENT 'refers to patient.id in pcr_patient databases',
+  service_id character(36) NOT NULL COMMENT 'refers to admin.service table',
+  person_id int NOT NULL COMMENT 'refers to person_record table',
   CONSTRAINT pk_patient_person_link PRIMARY KEY (patient_id)
-) COMMENT ''defines the current linkage between patient records and their person ID'';
+) COMMENT 'defines the current linkage between patient records and their person ID';
 
 CREATE INDEX ix_person_id ON patient_person_link (person_id);
 
@@ -36,7 +36,7 @@ CREATE TABLE patient_person_link_history
   new_person_id int NOT NULL,
   previous_person_id int,
   CONSTRAINT pk_patient_person_link_history PRIMARY KEY (patient_id, updated)
-) COMMENT ''stores the history of matched person ID for a patient ID'';
+) COMMENT 'stores the history of matched person ID for a patient ID';
 
 CREATE INDEX ix_updated ON patient_person_link_history (updated);
 
