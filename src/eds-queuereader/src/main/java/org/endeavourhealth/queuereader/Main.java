87187bfb23ff5a29ec00294a77834b2305f2df68
Drew Littler <drewlittler@hotmail.com>
2019-09-18 14:38:51 +0100

    Changed Frailty API to use newer way to get pseudo ID from patient ID

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -82,6 +82,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
+
 	public static void main(String[] args) throws Exception {
 
 		String configId = args[0];
