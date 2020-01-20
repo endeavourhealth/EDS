5574cc1af81ae6d810d8c1f135fec18f15141826
Drew Littler <drewlittler@hotmail.com>
2020-01-20 12:37:18 +0000

    Force build of queue readers and some DDS-UI changes to make service set up easier

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -100,6 +100,7 @@ import java.util.regex.Pattern;
 public class Main {
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
+
 	public static void main(String[] args) throws Exception {
 
 		String configId = args[0];
