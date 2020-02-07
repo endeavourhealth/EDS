78aeaa5c652475b36ebed5e0df887ea04d9d8803
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2020-02-07 15:19:23 +0000

    latest version of Barts staging tables and staging stored procs

--- src/database/mySQL/bartsStaging/create_tables_cds.sql
+++ src/database/mySQL/bartsStaging/create_tables_cds.sql
@@ -14,6 +14,7 @@ drop table if exists cds_emergency_target;
 drop table if exists cds_emergency_target_latest;
 drop table if exists cds_critical_care;
 drop table if exists cds_critical_care_latest;
+drop table if exists cds_critical_care_target;
 drop table if exists cds_home_delivery_birth;
 drop table if exists cds_home_delivery_birth_latest;
 drop table if exists cds_tail;
@@ -446,7 +447,7 @@ create table cds_emergency_target
   (
       exchange_id                	char(36)     NOT NULL COMMENT ' links to audit.exchange table (but on a different server)',
       unique_id                  	varchar(255) NOT NULL COMMENT ' unique ID derived from source IDs ',
-      is_delete                  	bool         NOT NULL COMMENT ' if this procedure should be deleted or upserted ',
+      is_delete                  	bool         NOT NULL COMMENT ' if this emergency encounter should be deleted or upserted ',
       person_id                  	int COMMENT ' person ID for the emergency encounter ',
       encounter_id               	int COMMENT ' encounter ID for the emergency encounter, derived from tail ',
       episode_id               	    int COMMENT ' episode ID associated with the emergency encounter, derived from tail ',
@@ -484,7 +485,7 @@ create table cds_emergency_target_latest
 (
     exchange_id                	char(36)     NOT NULL COMMENT ' links to audit.exchange table (but on a different server)',
     unique_id                  	varchar(255) NOT NULL COMMENT ' unique ID derived from source IDs ',
-    is_delete                  	bool         NOT NULL COMMENT ' if this procedure should be deleted or upserted ',
+    is_delete                  	bool         NOT NULL COMMENT ' if this emergency encounter should be deleted or upserted ',
     person_id                  	int COMMENT ' person ID for the emergency encounter ',
     encounter_id               	int COMMENT ' encounter ID for the emergency encounter, derived from tail ',
     episode_id               	int COMMENT ' episode ID associated with the emergency encounter, derived from tail ',
@@ -529,8 +530,8 @@ create table cds_critical_care
     nhs_number                     varchar(10)  NOT NULL COMMENT 'from NHSNumber',
 
     critical_care_type_id          varchar(2)   COMMENT '01 - Neonatal, 02 - Paediatric, 03 - Adult',
-    spell_number                   varchar(12),
-    episode_number                 varchar(2),
+    spell_number                   varchar(12)  COMMENT 'Links to the Inpatient spell_number with episode_number ',
+    episode_number                 varchar(2)   COMMENT 'Used to link to the Inpatient spell_number ',
     critical_care_identifier       varchar(20),
     care_start_date                datetime     COMMENT 'start date and time of care',
     care_unit_function             varchar(12),
@@ -579,8 +580,8 @@ create table cds_critical_care_latest
     nhs_number                     varchar(10)  NOT NULL COMMENT 'from NHSNumber',
 
     critical_care_type_id          varchar(2)   COMMENT '01 - Neonatal, 02 - Paediatric, 03 - Adult',
-    spell_number                   varchar(12),
-    episode_number                 varchar(2),
+    spell_number                   varchar(12)  COMMENT 'Links to the Inpatient spell_number with episode_number ',
+    episode_number                 varchar(2)   COMMENT 'Used to link to the Inpatient spell_number ',
     critical_care_identifier       varchar(20),
     care_start_date                datetime     COMMENT 'start date and time of care',
     care_unit_function             varchar(12),
@@ -617,6 +618,54 @@ create table cds_critical_care_latest
     CONSTRAINT pk_cds_critical_care_latest PRIMARY KEY (cds_unique_identifier, critical_care_identifier)
 );
 CREATE INDEX ix_cds_critical_care_latest_join_helper on cds_critical_care_latest (exchange_id, cds_unique_identifier, critical_care_identifier);
+
+create table cds_critical_care_target
+(
+    exchange_id                          char(36)     NOT NULL COMMENT ' links to audit.exchange table (but on a different server)',
+    unique_id                            varchar(255) NOT NULL COMMENT ' unique ID derived from source IDs ',
+    is_delete                            bool         NOT NULL COMMENT ' if this critical care encounter should be deleted or upserted ',
+    person_id                            int COMMENT ' person ID for the critical care encounter ',
+    performer_personnel_id     	         int COMMENT ' responsible personnel ID for the critical care encounter ',
+
+    critical_care_type_id                varchar(2) COMMENT '01 - Neonatal, 02 - Paediatric, 03 - Adult',
+    spell_number                         varchar(12) COMMENT 'Links to the Inpatient spell_number with episode_number ',
+    episode_number                       varchar(2) COMMENT 'Used to link to the Inpatient spell_number ',
+    critical_care_identifier             varchar(20) COMMENT 'Used to create the unique encounter Id for this encounter ',
+    care_start_date                      datetime COMMENT 'start date and time of care',
+    care_unit_function                   varchar(12),
+    admission_source_code                varchar(12) COMMENT 'LKP_CDS_ADMISS_SOURCE',
+    admission_type_code                  varchar(12),
+    admission_location                   varchar(12),
+
+    gestation_length_at_delivery         varchar(2),
+    advanced_respiratory_support_days    smallint,
+    basic_respiratory_supports_days      smallint,
+    advanced_cardiovascular_support_days smallint,
+    basic_cardiovascular_support_days    smallint,
+    renal_support_days                   smallint,
+    neurological_support_days            smallint,
+    gastro_intestinal_support_days       smallint,
+    dermatological_support_days          smallint,
+    liver_support_days                   smallint,
+    organ_support_maximum                smallint,
+    critical_care_level2_days            smallint,
+    critical_care_level3_days            smallint,
+
+    discharge_date                       datetime COMMENT 'date and time of actual discharge',
+    discharge_ready_date                 datetime COMMENT 'date and time of when patient was ready for discharge',
+    discharge_status_code                varchar(12),
+    discharge_destination                varchar(12) COMMENT 'LKP_CDS_DISCH_DEST',
+    discharge_location                   varchar(12),
+
+    -- store any care activity data
+    care_activity_1                      varchar(135) COMMENT 'made up of date, weight, upto 20 two digit activity codes and upto 20 4 digit OPCS code - total fixed length 135',
+    care_activity_2100                   mediumtext COMMENT 'same format as care_activity_1 for a further 99 times',
+
+    audit_json                  mediumtext   null comment 'Used for Audit Purposes',
+    is_confidential             bool COMMENT 'if this procedure should be confidential or not, i.e. withheld flag set',
+
+    CONSTRAINT pk_critical_care_target PRIMARY KEY (exchange_id, unique_id, critical_care_identifier)
+);
 
 -- records from sus home delivery and birth files are written to this table (similar to Inpatient structure)
 create table cds_home_delivery_birth
--- src/database/mySQL/ehr/create_tables.sql
+++ src/database/mySQL/ehr/create_tables.sql
@@ -2,6 +2,8 @@ USE ehr_??; -- we can have multiple EHR databases
 
 DROP TABLE IF EXISTS resource_history;
 DROP TABLE IF EXISTS resource_current;
+DROP TABLE IF EXISTS encounter_history;
+DROP TABLE IF EXISTS encounter_current;
 
 CREATE TABLE resource_history (
   service_id char(36),
@@ -56,3 +58,65 @@ ON resource_current (resource_id, resource_type, resource_checksum);
 -- index used to prevent resources being duplicated if the patient ID changed
 CREATE UNIQUE INDEX uix_resource_current_id_type
 ON resource_current (resource_id, resource_type);
+
+
+CREATE TABLE encounter_history (
+    service_id char(36),
+    composition_resource_id char(36),
+    created_at datetime,
+
+    encounter_id char(36),
+    patient_id varchar(36),
+    practitioner_id char(36),
+    appointment_id char(36),
+    effective_date datetime,
+    effective_end_date datetime,
+    episode_of_care_id char(36),
+    service_provider_organisation_id char(36),
+    encounter_type char(50),
+    parent_encounter_id char(36),
+    additional_fields_json mediumtext,
+
+    composition_encounter_checksum bigint,
+    is_deleted boolean,
+    exchange_batch_id char(36),
+    CONSTRAINT pk_encounter_history PRIMARY KEY (composition_resource_id, encounter_id, created_at DESC)
+)
+ROW_FORMAT=COMPRESSED
+KEY_BLOCK_SIZE=8;
+
+-- index used to get data out for subscriber feeds
+CREATE INDEX ix_encounter_history_batch_id
+    ON encounter_history (exchange_batch_id, composition_resource_id, encounter_id, created_at);
+
+
+CREATE TABLE encounter_current (
+    service_id char(36),
+    composition_resource_id char(36),
+    updated_at datetime,
+
+    encounter_id char(36),
+    patient_id varchar(36),
+    practitioner_id char(36),
+    appointment_id char(36),
+    effective_date datetime,
+    effective_end_date datetime,
+    episode_of_care_id char(36),
+    service_provider_organisation_id char(36),
+    encounter_type char(50),
+    parent_encounter_id char(36),
+    additional_fields_json mediumtext,
+
+    composition_encounter_checksum bigint,
+    CONSTRAINT pk_encounter_current PRIMARY KEY (service_id, patient_id, composition_resource_id, encounter_id)
+)
+ROW_FORMAT=COMPRESSED
+KEY_BLOCK_SIZE=8;
+
+-- index used to retrieve specific composition resources for a service
+CREATE INDEX ix_encounter_current_service_id
+    ON encounter_current (service_id, composition_resource_id);
+
+-- index used to retrieve current version of a composition encounter and check its checksum to work out if it has changed
+CREATE INDEX ix_encounter_current_id_type_checksum
+    ON encounter_current (composition_resource_id, encounter_id, composition_encounter_checksum);
