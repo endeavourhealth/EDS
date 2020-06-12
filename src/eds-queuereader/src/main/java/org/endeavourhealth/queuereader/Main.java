85a20c895bfc95254e2b7433a098780ee4540a24
Drew Littler <drewlittler@hotmail.com>
2020-06-12 17:18:32 +0100

    Adding routine to delete any FHIR resources for deleted patient records

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -80,7 +80,6 @@ import static org.endeavourhealth.core.xml.QueryDocument.ServiceContractType.PUB
 public class Main {
 	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-
 	public static void main(String[] args) throws Exception {
 
 		String configId = args[0];
