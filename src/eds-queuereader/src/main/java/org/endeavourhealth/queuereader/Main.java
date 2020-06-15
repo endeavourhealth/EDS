cd49ade9c15938865b1fbae7fb83a33f1e038bc2
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2020-06-15 18:35:54 +0100

    Fix to createDeleteZipsForSubscriber to get build working

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/Main.java
@@ -1842,7 +1842,7 @@ public class Main {
 				org.endeavourhealth.transform.subscriber.targetTables.Observation obsWriter = container.getObservations();
 
 				for (Long id: ids) {
-					SubscriberId idWrapper = new SubscriberId(SubscriberTableId.OBSERVATION.getId(), id.longValue(), null);
+					SubscriberId idWrapper = new SubscriberId(SubscriberTableId.OBSERVATION.getId(), id.longValue(), null, null);
 					obsWriter.writeDelete(idWrapper);
 				}
 
