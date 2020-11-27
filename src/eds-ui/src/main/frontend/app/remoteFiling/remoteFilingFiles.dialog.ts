0ad7f356c462bbb146547b2bc58d6904217eabcf
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2020-11-27 14:48:07 +0000

    dds-ui remote filing monitoring updates - removed logging and finalised zip file JS download

--- src/eds-ui/src/main/frontend/app/remoteFiling/remoteFilingFiles.dialog.ts
+++ src/eds-ui/src/main/frontend/app/remoteFiling/remoteFilingFiles.dialog.ts
@@ -4,7 +4,6 @@ import {NgbModal, NgbActiveModal, NgbModalOptions} from "@ng-bootstrap/ng-bootst
 
 import {SubscriberZipFileUUID} from "./models/SubscriberZipFileUUID";
 import {RemoteFilingService} from "./remoteFiling.service";
-import { saveAs } from 'file-saver';
 
 @Component({
     selector: 'ngbd-modal-content',
@@ -41,45 +40,14 @@ export class RemoteFilingFilesDialog  {
     }
 
     downloadZipData(messageUUID: string, data: string) {
-
-        const vm = this;
-
-        let zip = require('jszip');
-
-        //var decodedStr = atob(messageUUID);
-        zip.loadAsync(data, {base64: true})
-            .then(function (blob) {
-                // will be called, even if content is corrupted
-                    //let FileSaver = require('file-saver');
-
-                    saveAs(blob,  messageUUID + ".zip");
-                    console.log('saveAs completed');
-            },
-                function (error) {
-                    vm.log.error('Failed to create zip file', error, 'Zip file')
-                    console.log('error calling loadAsync')
-                }
-            )
-    }
-
-    downloadZipData2(messageUUID: string, data: string) {
-
-        //var decodedStr = atob(data);
-
         const link = document.createElement('a');
         link.setAttribute('target', '_blank');
         link.setAttribute('href', 'data:application/zip;base64,'+data);
-
-        var fileName = messageUUID + '.zip';
-        link.setAttribute('download', fileName);
+        link.setAttribute('download', messageUUID + '.zip');
         document.body.appendChild(link);
         link.click();
         link.remove();
      }
-
-    createZipDownloadFileName (messageUUID: string) {
-        return messageUUID + '.zip';
-    }
 
     messageDialogLarge(title: string, message: string) {
         MessageBoxDialog.openLarge(
@@ -113,7 +81,6 @@ export class RemoteFilingFilesDialog  {
         if (vm.view.endsWith('filing history (all)')) {
             return true;
         }
-
         return false;
     }
 }
