01dccef639da9deba03cde84a52e46ce78c15dd5
Drew Littler <drewlittler@hotmail.com>
2021-02-16 17:11:09 +0000

    Adding new routine to fix SD-367

--- src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/routines/SD367.java
+++ src/eds-queuereader/src/main/java/org/endeavourhealth/queuereader/routines/SD367.java
@@ -2746,7 +2746,13 @@ public class SD367 extends AbstractRoutine {
             //so it doesn't matter that we prefer Vision over Emis
             EthnicCategory ec = VisionMappingHelper.findEthnicityCode(code);
             if (ec == null) {
-                ec = findEmisEthnicityCode(code);
+                try {
+                    ec = findEmisEthnicityCode(code);
+                } catch (Exception ex) {
+                    if (ex.getMessage() == null || !ex.getMessage().contains("Unknown ethnicity code")) {
+                        throw ex;
+                    }
+                }
             }
             return ec;
 
