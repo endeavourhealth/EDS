5c76b46e3a52b558f9747e14f8f81d8dcbc52949
Drew Littler <drewlittler@hotmail.com>
2021-03-09 16:03:15 +0000

    Fix to SQL Server compass v2 script

--- src/database/SQL Server/compass v2 (subscriber)/create_tables_NO_FKS.sql
+++ src/database/SQL Server/compass v2 (subscriber)/create_tables_NO_FKS.sql
@@ -424,7 +424,7 @@ CREATE TABLE [observation_additional] (
   [id] bigint NOT NULL,
   [property_id] int NOT NULL, -- IM dbid reference
   [value_id] int NULL,
-  [json_value] json NULL,
+  [json_value] nvarchar(max) NULL,
   PRIMARY KEY ([id], [property_id])
 )
 GO
